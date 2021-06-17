fdb7024edc0aa381e44dcd2863725130f993c08a
Olaleye Blessing <Olayinkablexxy@gmail.com>
2021-06-17 20:09:11 +0100

    add cors package for cross origin request

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.json({ limit: "30mb" }));
 
-// app.use(cors());
+app.use(cors());
 
 app.use(
     cors({
