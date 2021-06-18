11c943a3a44cfe8fb4218ed78b215089009ac2e5
Olaleye Blessing <Olayinkablexxy@gmail.com>
2021-06-18 08:28:21 +0100

    set cookie secure option  to true for production mode

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -20,7 +20,8 @@ const sendToken = (user, statusCode, res) => {
             Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
-        secure: nodeEnv !== "development",
+        // secure: nodeEnv !== "development",
+        secure: true,
     };
 
     res.cookie("jwt", token, cookieOptions);
